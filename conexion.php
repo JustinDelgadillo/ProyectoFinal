aac893064eb984258ff55c59a1ef3bc57b8caac8
JustinDelgadillo <justinmarcos81@aragon.unam.mx>
2023-11-27 21:40:32 -0600

    Establecimiento de la conexión y manejo de errores

--- conexion.php
+++ conexion.php
@@ -1,6 +1,12 @@
 <?php
-$servername = "sql300.infinityfree.com";
-$username = "if0_35205189";
-$password = "lYiYoCi1BqNg";
-$dbname = "if0_35205189_registros";
+$servername = "sql300.infinityfree.com";  
+$username = "if0_35205189";    
+$password = "lYiYoCi1BqNg"; 
+$dbname = "if0_35205189_registros"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 ?>
