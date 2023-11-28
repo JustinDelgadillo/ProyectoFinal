61e0cbdd5eb5480a18463a007acf28cb45f45414
JustinDelgadillo <justinmarcos81@aragon.unam.mx>
2023-11-27 21:52:41 -0600

    Estructura HTML básica y enlace a estilos

---
+++ mostrar_registros.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>ProyectoFinal - Ver Registros</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+    <h1>Usuarios Registrados</h1>
